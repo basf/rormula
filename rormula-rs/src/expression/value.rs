889c662ba4c8aadf78d334ac99b554ecfa089ab4
Behrang Shafei <50267830+bertiqwerty@users.noreply.github.com>
2024-03-15 08:16:54 +0100

    removed wrong comment

--- rormula-rs/src/expression/value.rs
+++ rormula-rs/src/expression/value.rs
@@ -4,7 +4,6 @@ use crate::{array::Array2d, result::RoErr, roerr};
 
 #[derive(Clone, Debug, PartialEq)]
 pub enum Value {
-    /// Vec<String> are the names of the columns, i.e., the resulting names of the new features
     Array(Array2d),
     RowInds(Vec<usize>),
     /// String is the name of the categorical
